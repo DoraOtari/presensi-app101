aad267cad0ad462788d99a712fd616e8e1273ce4
DoraOtari <doraotari@gmail.com>
2023-12-19 08:41:24 +0700

    pertemuan 7 part 1

--- resources/views/components/template.blade.php
+++ resources/views/components/template.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>{{ config("app.name") }}</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
         @livewireStyles
         @livewireScripts
@@ -40,53 +40,58 @@
                         ></button>
                     </div>
                     <div class="offcanvas-body">
-                        <ul class="navbar-nav justify-content-start flex-grow-1 pe-3">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="{{ url('/dashboard') }}" ><i class="bi bi-house"></i> Dashboard</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ url('/profil') }}"><i class="bi bi-person-bounding-box"></i> Profil</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a
-                                    class="nav-link dropdown-toggle"
-                                    href="#"
-                                    id="dropdownId"
-                                    data-bs-toggle="dropdown"
-                                    aria-haspopup="true"
-                                    aria-expanded="false"
-                                    >Dropdown</a
-                                >
-                                <div
-                                    class="dropdown-menu"
-                                    aria-labelledby="dropdownId"
-                                >
-                                    <a class="dropdown-item" href="#"
-                                        >Action 1</a
+                        @auth
+                            <ul class="navbar-nav nav-underline justify-content-start flex-grow-1 pe-3">
+                                <li class="nav-item">
+                                    <a class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}" href="{{ url('/dashboard') }}" ><i class="bi bi-house"></i> Dashboard</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link {{ Request::is('profil') ? 'active' : '' }}" href="{{ url('/profil') }}"><i class="bi bi-person-bounding-box"></i> Profil</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a
+                                        class="nav-link dropdown-toggle"
+                                        href="#"
+                                        id="dropdownId"
+                                        data-bs-toggle="dropdown"
+                                        aria-haspopup="true"
+                                        aria-expanded="false"
+                                        >Dropdown</a
                                     >
-                                    <a class="dropdown-item" href="#"
-                                        >Action 2</a
+                                    <div
+                                        class="dropdown-menu"
+                                        aria-labelledby="dropdownId"
                                     >
-                                </div>
-                            </li>
-                        </ul>
-                        <form class="d-flex">
-                            <input
-                                class="form-control me-2"
-                                type="search"
-                                placeholder="Search"
-                                aria-label="Search"
-                            />
-                            <button class="btn btn-outline-success" type="submit">
-                                Search
-                            </button>
-                        </form>
+                                        <a class="dropdown-item" href="#"
+                                            >Action 1</a
+                                        >
+                                        <a class="dropdown-item" href="#"
+                                            >Action 2</a
+                                        >
+                                    </div>
+                                </li>
+                            </ul>
+                            <div class="hstack gap-2">
+                                <img src="https://placehold.co/35" class="img-thumbnail rounded-circle">
+                                <small class="lead">Guest</small>
+                                <div class="vr"></div>
+                                <form action="{{ route('logout') }}" method="post">
+                                    <button type="submit" class="btn btn-sm btn-danger rounded-circle">
+                                        <i class="bi-power"></i>
+                                    </button>
+                                </form>
+                            </div>
+                        @else
+                            <a class="ms-auto" href="{{ route('register') }}">
+                                <i class="bi-person-plus"></i> Register
+                            </a>
+                        @endauth
                     </div>
                 </div>
             </div>
         </nav>
         
         {{ $slot }}
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
